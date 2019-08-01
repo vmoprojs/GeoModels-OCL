0c3c0c667de203c94ce6a4e893969a2cb5e2dc4f
vmoprojs <victor.morales@uv.cl>
2019-08-01 07:05:12 -0500

    new version

--- src/CompositeLikelihood_OCL.c
+++ src/CompositeLikelihood_OCL.c
@@ -39,8 +39,6 @@ void Comp_Pair_Gauss2_OCL(int *cormod, double *coordx, double *coordy, double *c
     if(!R_FINITE(*res))*res = LOW;
 }
 
-
-
 // Composite marginal (difference) log-likelihood for the spatial Gaussian model:
 void Comp_Diff_Gauss2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data, int *NN,
                       double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
@@ -91,8 +89,6 @@ void Comp_Pair_SinhGauss2_OCL(int *cormod, double *coordx, double *coordy, doubl
     if(!R_FINITE(*res))*res = LOW;
 }
 
-
-
 void Comp_Pair_LogGauss2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                               double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
@@ -130,7 +126,7 @@ void Comp_Pair_SkewGauss2_OCL(int *cormod, double *coordx, double *coordy, doubl
                               double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
     if(nuis[1]<0 || nuis[2]<0|| nuis[3]<0
-       || CheckCor(cormod,par)==-2) {*res=LOW;  return;}
+        ) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_Gamma2_OCL";
     
     int *int_par;
@@ -293,7 +289,6 @@ void Comp_Pair_Weibull2_OCL(int *cormod, double *coordx, double *coordy, double 
     
     double sill=1-nuis[0];
     if(nuis[2]<=0||sill<0||sill>1||CheckCor(cormod,par)==-2) {*res=LOW;  return;}
-       // if(nuis[2]<=0||sill<0||sill>1) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_Weibull2_OCL";
     int *int_par;
     double *dou_par;
@@ -334,23 +329,18 @@ void Comp_Pair_TWOPIECET2_OCL(int *cormod, double *coordx, double *coordy, doubl
     double eta=nuis[3];  //skewness parameter
     double sill=nuis[2];
     double df=nuis[0];
-    
     if( fabs(eta)>1|| sill<0||df >0.5||df<0) {*res=LOW;  return;}
-    
     char *f_name = "Comp_Pair_TWOPIECET2_OCL";
-    
     int *int_par;
     double *dou_par;
      int_par = (int*)Calloc((50), int *);
     dou_par = (double*)Calloc((50), double *);
     param_OCL(cormod,NN,par,weigthed,nuis,int_par,dou_par);
-    
     exec_kernel(coordx,coordy, mean,data, int_par, dou_par, local_wi,dev,res,f_name);
      Free(int_par);
     Free(dou_par);
     if(!R_FINITE(*res))*res = LOW;
 }
-
 
 void Comp_Pair_TWOPIECEGauss2_OCL(int *cormod, double *coordx, double *coordy, double *coordt, double *data, int *NN,
                               double *par,  int *weigthed,double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,
@@ -371,16 +361,12 @@ void Comp_Pair_TWOPIECEGauss2_OCL(int *cormod, double *coordx, double *coordy, d
     if(!R_FINITE(*res))*res = LOW;
 }
 
-
-
 void Comp_Pair_Kumaraswamy2_OCL(int *cormod, double *coordx, double *coordy, double *coordt, double *data, int *NN,
                                   double *par,  int *weigthed,double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,
                                   int *local_wi, int *dev)
 {
     if(nuis[2]<0||nuis[3]<0||CheckCor(cormod,par)==-2) {*res=LOW;  return;}
-    
     char *f_name = "Comp_Pair_Kumaraswamy2_OCL";
-    
     int *int_par;
     double *dou_par;
       int_par = (int*)Calloc((50), int *);
@@ -399,10 +385,8 @@ void Comp_Pair_Tukeyh2_OCL(int *cormod, double *coordx, double *coordy, double *
     double sill=nuis[1];
     double nugget=nuis[0];
     double tail=nuis[2];
-    if( sill<0||tail<0||nugget>=1||nugget<0|| CheckCor(cormod,par)==-2){*res=LOW; return;}
-    
+    if( sill<0||tail<0||nugget>=1||nugget<0 ){*res=LOW; return;}
     char *f_name = "Comp_Pair_Tukeyh2_OCL";
-    
     int *int_par;
     double *dou_par;
     int_par = (int*)Calloc((50), int *);
@@ -421,9 +405,7 @@ void Comp_Pair_Tukeyh2_OCL(int *cormod, double *coordx, double *coordy, double *
 /******************************************************************************************/
 /******************************************************************************************/
 /******************************************************************************************/
-/******************************************************************************************/
-/******************************************************************************************/
-/******************************************************************************************/
+
 // Composite marginal (pariwise) log-likelihood for the spatial-temporal Gaussian model:
 
 void Comp_Pair_Gauss_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
@@ -442,8 +424,6 @@ void Comp_Pair_Gauss_st2_OCL(int *cormod, double *coordx, double *coordy, double
     Free(dou_par);
     if(!R_FINITE(*res))*res = LOW;
 }
-
-
 void Comp_Pair_WrapGauss_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                              double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
@@ -461,13 +441,11 @@ void Comp_Pair_WrapGauss_st2_OCL(int *cormod, double *coordx, double *coordy, do
     if(!R_FINITE(*res))*res = LOW;
 }
 
-
 void Comp_Pair_PoisbinGauss_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                                  double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
     if( nuis[0]>1 || nuis[0]<0){*res=LOW; return;}
     char *f_name = "Comp_Pair_PoisbinGauss_st2_OCL";
-    
     int *int_par;
     double *dou_par;
    int_par = (int*)Calloc((50), int *);
@@ -503,7 +481,6 @@ void Comp_Cond_Gauss_st2_OCL(int *cormod, double *coordx, double *coordy, double
 {
     //if(nuis[1]<0 || nuis[0]<0) {*res=LOW;  return;}
     char *f_name = "Comp_Cond_Gauss_st2_OCL";
-    
     int *int_par;
     double *dou_par;
       int_par = (int*)Calloc((50), int *);
@@ -521,7 +498,6 @@ void Comp_Diff_Gauss_st2_OCL(int *cormod, double *coordx, double *coordy, double
 {
     //if(nuis[1]<0 || nuis[0]<0) {*res=LOW;  return;}
     char *f_name = "Comp_Diff_Gauss_st2_OCL";
-    
     int *int_par;
     double *dou_par;
        int_par = (int*)Calloc((50), int *);
@@ -537,7 +513,7 @@ void Comp_Diff_Gauss_st2_OCL(int *cormod, double *coordx, double *coordy, double
 void Comp_Pair_SkewGauss_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                              double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
-    //if(nuis[1]<0 || nuis[0]<0) {*res=LOW;  return;}
+    if(nuis[1]<0 || nuis[0]<0) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_SkewGauss_st2_OCL";
     
     int *int_par;
@@ -556,7 +532,7 @@ void Comp_Pair_SkewGauss_st2_OCL(int *cormod, double *coordx, double *coordy, do
 void Comp_Pair_SinhGauss_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                                  double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
-    //if(nuis[1]<0 || nuis[0]<0|| nuis[3]<0) {*res=LOW;  return;}
+    if(nuis[1]<0 || nuis[0]<0|| nuis[3]<0) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_SinhGauss_st2_OCL";
     
     int *int_par;
@@ -575,7 +551,7 @@ void Comp_Pair_Gamma_st2_OCL(int *cormod, double *coordx, double *coordy, double
                                  double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
     double sill=1-nuis[0];
-    if(nuis[2]<1||sill<0||sill>1||CheckCor(cormod,par)==-2) {*res=LOW;  return;}
+    if(nuis[2]<1||sill<0||sill>1) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_Gamma_st2_OCL";
     
     int *int_par;
@@ -683,16 +659,12 @@ void Comp_Pair_Logistic_st2_OCL(int *cormod, double *coordx, double *coordy, dou
 }
 
 
-
-
 void Comp_Pair_Weibull_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                              double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
     double sill=1-nuis[0];
-    if(nuis[2]<=0||sill<0||sill>1||CheckCor(cormod,par)==-2) {*res=LOW;  return;}
-       // if(nuis[2]<=0||sill<0||sill>1) {*res=LOW;  return;}
+    if(nuis[2]<=0||sill<0||sill>1) {*res=LOW;  return;}
     char *f_name = "Comp_Pair_Weibull_st2_OCL";
-    
     int *int_par;
     double *dou_par;
     int_par = (int*)Calloc((50), int *);
@@ -705,19 +677,13 @@ void Comp_Pair_Weibull_st2_OCL(int *cormod, double *coordx, double *coordy, doub
 }
 
 
-
-
-
-
-
 void Comp_Pair_T_st2_OCL(int *cormod, double *coordx, double *coordy, double *coordt,double *data,int *NN,
                                double *par, int *weigthed, double *res,double *mean,double *mean2,double *nuis,int *ns, int *NS,int *local_wi, int *dev)
 {
     double sill=nuis[2];
     double df=nuis[0];
-    if( sill<0||df<0||df>0.5||CheckCor(cormod,par)==-2){*res=LOW; return;}
+    if( sill<0||df<0||df>0.5){*res=LOW; return;}
     char *f_name = "Comp_Pair_T_st2_OCL";
-    
     int *int_par;
     double *dou_par;
    int_par = (int*)Calloc((50), int *);
@@ -802,7 +768,7 @@ void Comp_Pair_Tukeyh_st2_OCL(int *cormod, double *coordx, double *coordy, doubl
     double sill=nuis[1];
     double nugget=nuis[0];
     double tail=nuis[2];
-    if( sill<0||tail<0||nugget>=1||nugget<0|| CheckCor(cormod,par)==-2){*res=LOW; return;}
+    if( sill<0||tail<0||nugget>=1||nugget<0){*res=LOW; return;}
     char *f_name = "Comp_Pair_Tukeyh_st2_OCL";
     
     int *int_par;
